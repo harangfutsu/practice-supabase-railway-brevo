bd048102d6905bda6f0e9e47fa6438e83a313d18
harangfutsu <harangfutsu@gmail.com>
2025-11-24 08:38:46 +0700

    feat: brevo

--- src/services/mailer.service.js
+++ src/services/mailer.service.js
@@ -1,38 +1,38 @@
-const { Resend } = require("resend");
+// src/services/mailer.service.js
 require("dotenv").config();
+const brevo = require("@getbrevo/brevo");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const apiInstance = new brevo.TransactionalEmailsApi();
+apiInstance.setApiKey(
+  brevo.TransactionalEmailsApiApiKeys.apiKey,
+  process.env.BREVO_API_KEY
+);
 
 const sendVerificationEmail = async (to, token) => {
   const verifyUrl = `${process.env.APP_URL}/verify-email?token=${token}`;
 
-  const mailOptions = {
-    from: process.env.MAILER_DEFAULT_SENDER_EMAIL || "onboarding@resend.dev",
-    to,
+  const emailData = {
+    sender: {
+      name: "EduCourse",
+      email: process.env.MAILER_DEFAULT_SENDER_EMAIL,
+    },
+    to: [{ email: to }],
     subject: "Verifikasi Akun EduCourse",
-    html: `
+    htmlContent: `
       <h3>Selamat datang di EduCourse!</h3>
       <p>Terima kasih sudah mendaftar. Klik tautan berikut untuk memverifikasi akun kamu:</p>
       <a href="${verifyUrl}" target="_blank">${verifyUrl}</a>
       <br/><br/>
       <p>Jika kamu tidak merasa mendaftar, abaikan email ini.</p>
-    `
+    `,
   };
 
   try {
-    const { data, error } = await resend.emails.send(mailOptions);
-
-    if (error) {
-      console.error("Gagal kirim email:", error);
-      return false;
-    }
-
-    console.log("data resend :", data)
-    console.log("Email terkirim. ID:", data.id);
+    const response = await apiInstance.sendTransacEmail(emailData);
+    console.log("Email terkirim. Message ID:", response.messageId);
     return true;
-
-  } catch (err) {
-    console.error("Resend error:", err);
+  } catch (error) {
+    console.error("Brevo error:", error.response?.body || error);
     return false;
   }
 };
