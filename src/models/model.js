c82e309aef3e8ce15e6c245ae114dac9aa545d9a
harangfutsu <harangfutsu@gmail.com>
2025-11-25 14:36:26 +0700

    group by course

--- src/models/model.js
+++ src/models/model.js
@@ -1,9 +1,8 @@
-const pool = require('../database/connection')
-const crypto = require('crypto')
+const pool = require("../config/db")
 
-// GET ALL COURSE
-const getAllCourse = ({ category, language, search, sortBy, order, limit, page }) =>
-    new Promise((resolve, reject) => {
+const getAllCourse = ({ category, language, search, sortBy, order, limit, page }) => {
+    return new Promise((resolve, reject) => {
+
         let sql = `
             SELECT 
                 c.course_id,
@@ -16,7 +15,8 @@ const getAllCourse = ({ category, language, search, sortBy, order, limit, page }
                 COUNT(f.rating) AS total_reviews
             FROM courses c
             LEFT JOIN feedbacks f ON c.course_id = f.id_course
-        `
+        `;
+
         const values = [];
         const conditions = [];
 
@@ -72,64 +72,10 @@ const getAllCourse = ({ category, language, search, sortBy, order, limit, page }
 
         pool.query(sql, values)
             .then(res => resolve(res.rows))
-            .catch(err => reject(err))
-    })
-
-
-// CREATE COURSE
-const createCourse = (title, category, description, price, language) =>
-    new Promise((resolve, reject) => {
-        const sql = `
-            INSERT INTO courses (course_id, title, category, description, price, language)
-            VALUES ($1, $2, $3, $4, $5, $6)
-        `
-        const values = [crypto.randomUUID(), title, category, description, price, language]
-
-        pool.query(sql, values)
-            .then(res => resolve(res))
-            .catch(err => reject(err))
-    })
-
-
-// UPDATE COURSE
-const updateCourse = (id, title, category, description, price, language) =>
-    new Promise((resolve, reject) => {
-        const sql = `
-            UPDATE courses
-            SET title = $1, category = $2, description = $3, price = $4, language = $5
-            WHERE course_id = $6
-        `
-        const values = [title, category, description, price, language, id]
-
-        pool.query(sql, values)
-            .then(res => resolve(res))
-            .catch(err => reject(err))
-    })
-
-
-// DELETE COURSE
-const deleteCourse = (id) =>
-    new Promise((resolve, reject) => {
-        const sql = `DELETE FROM courses WHERE course_id = $1`
-        pool.query(sql, [id])
-            .then(res => resolve(res))
-            .catch(err => reject(err))
-    })
-
-
-// GET BY ID
-const getCourseById = (id) =>
-    new Promise((resolve, reject) => {
-        const sql = `SELECT * FROM courses WHERE course_id = $1`
-        pool.query(sql, [id])
-            .then(res => resolve(res.rows[0]))
-            .catch(err => reject(err))
-    })
+            .catch(err => reject(err));
+    });
+};
 
 module.exports = {
-    getAllCourse,
-    createCourse,
-    updateCourse,
-    deleteCourse,
-    getCourseById
-}
+    getAllCourse
+};
